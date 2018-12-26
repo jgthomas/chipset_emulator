d0f5397913d30b8a86e3c15a953896632bcc7005
James Thomas <codeactor@protonmail.com>
2018-12-26 12:20:11 -0500

    register init added

--- core/demo.c
+++ core/demo.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 #include "chipset.h"
 
 chipset *init_chipset(double bits);
 void delete_chipset(chipset *chip);
+void init_registers(chipset *chip);
 int memory_size(double bits);
 
 
@@ -20,15 +22,29 @@ int main(void)
 
 chipset *init_chipset(double bits)
 {
+        int memory = memory_size(bits);
         chipset *chip = malloc(sizeof(chip));
-        chip->MEMORY = malloc(sizeof(int) * (int) bits);
+        chip->MEMORY = malloc(sizeof(int) * memory);
+        chip->INSTRUCTIONS = malloc(sizeof(instruction) * memory);
+        init_registers(chip);
+        chip->EXECUTE = true;
         return chip;
+}
+
+
+void init_registers(chipset *chip)
+{
+        chip->PC = 0;
+        chip->IR = 0;
+        chip->R0 = 0;
+        chip->R1 = 0;
 }
 
 
 void delete_chipset(chipset *chip)
 {
         free(chip->MEMORY);
+        free(chip->INSTRUCTIONS);
         free(chip);
 }
 
