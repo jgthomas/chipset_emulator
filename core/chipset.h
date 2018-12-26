3c595290cfd5a0b14a402d572e8cf5f1b365628b
James Thomas <codeactor@protonmail.com>
2018-12-26 11:59:17 -0500

    move struct typedef into header

--- core/chipset.h
+++ core/chipset.h
@@ -1,6 +1,18 @@
 #ifndef CHIPSET_H
 #define CHIPSET_H
 
-typedef struct chipset chipset;
+#include <stdbool.h>
+
+typedef void (*instruction)(void);
+
+typedef struct chipset {
+        int PC;
+        int IR;
+        int R0;
+        int R1;
+        int *MEMORY;
+        bool EXECUTE;
+        instruction *INSTRUCTIONS;
+} chipset;
 
 #endif
--- core/demo.c
+++ core/demo.c
@@ -3,7 +3,8 @@
 #include <math.h>
 #include "chipset.h"
 
-//chipset init_chipset();
+chipset *init_chipset(double bits);
+void free_chipset(chipset *chip);
 int memory_size(double bits);
 
 
@@ -12,13 +13,22 @@ int main(void)
         printf("%d\n", memory_size(4));
         printf("%d\n", memory_size(8));
         printf("%d\n", memory_size(16));
+        init_chipset(4);
 }
 
 
-//chipset init_chipset()
-//{
-//        chipset *chip = malloc(sizeof(chip));
-//}
+chipset *init_chipset(double bits)
+{
+        chipset *chip = malloc(sizeof(chip));
+        chip->MEMORY = malloc(sizeof(int*) * (int) bits);
+        return chip;
+}
+
+
+void free_chipset(chipset *chip)
+{
+        free(chip);
+}
 
 
 int memory_size(double bits)
