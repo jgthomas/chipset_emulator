e1737f12a469df353a655359add060c538b4aa9a
James Thomas <codeactor@protonmail.com>
2018-12-27 08:24:50 -0500

    test swap in demo

--- core/demo.c
+++ core/demo.c
@@ -8,7 +8,7 @@ int main(void)
 {
         int bits = 4;
         int instructions = 16;
-        char *program = "03 03 03 04 04 04 04";
+        char *program = "03 03 03 04 04 04 04 07";
 
         chipset *chip = init_chipset(bits);
         load_instructions(chip, instructions);
