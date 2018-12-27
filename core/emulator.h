7474ea58688955c61ab31d74b6585fac191e2c90
James Thomas <codeactor@protonmail.com>
2018-12-27 09:55:09 -0500

    delete functions only used internally

--- core/emulator.h
+++ core/emulator.h
@@ -3,12 +3,8 @@
 
 #include "chipset.h"
 
-//chipset *init_chipset(int bits);
 chipset *init_chip(int bits, int instruction_count);
-//void load_instructions(chipset *chip, int count);
-//void load_program(chipset *chip, char *program);
 int execute(chipset *chip, char *program);
-//void execute_program(chipset *chip);
 int debug(chipset *chip, char *program);
 void delete_chipset(chipset *chip);
 
