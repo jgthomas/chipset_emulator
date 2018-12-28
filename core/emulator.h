be50b0956e5dd7b8515d8e3bf6ac44e1cf8a50c1
James Thomas <codeactor@protonmail.com>
2018-12-28 12:35:03 -0500

    add convert_code to the header file

--- core/emulator.h
+++ core/emulator.h
@@ -7,5 +7,6 @@ chipset *init_chip(int bits, int instruction_count);
 int execute(chipset *chip, char *program);
 int debug(chipset *chip, char *program);
 void delete_chipset(chipset *chip);
+int convert_code(char *hex, int bits);
 
 #endif
