60b76462a96fd706e1a92e0f98822d4b03e3edf2
James Thomas <codeactor@protonmail.com>
2019-10-29 19:54:12 +0000

    switch to cmocka for tests

--- tests/emulator_test.c
+++ tests/emulator_test.c
@@ -2,14 +2,16 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <BCUnit/Basic.h>
-#include <BCUnit/BCUnit.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
 #include "chipset.h"
 #include "instructions.h"
 #include "emulator.h"
 
 
-void testANSWER_THREE(void)
+void test_answer_three(void **state)
 {
         char *tests[] = {"03 03 03 00",
                          "04 04 04 07 00",
@@ -27,44 +29,14 @@ void testANSWER_THREE(void)
         for (int i = 0; i < num_tests; i++)
         {
                 int result = execute(chip, tests[i]);
-                CU_ASSERT_TRUE(answer == result);
+                assert_int_equal(answer, result);
         }
 
         delete_chipset(chip);
 }
 
 
-/**
- * Each test features an instruction in turn
- *
- * */
-void testUSING_ALL_INSTRUCTIONS(void)
-{
-        char *tests[] = {"03 00",
-                         "04 04 04 01 00",
-                         "04 04 04 04 04 01 05 00",
-                         "03 03 07 01 01 07 00",
-                         "09 03 00 07 00",
-                         "0a 04 07 00 0f 00",
-                         "03 03 03 03 0b 0f 05 05 05 05 09 0f 00 00"};
-
-        int num_tests = sizeof(tests) / sizeof(tests[0]);
-
-        int answer[] = {1,3,4,2,7,15,4};
-
-        chipset *chip = init_chip(4, 16);
-
-        for (int i = 0; i < num_tests; i++)
-        {
-                int result = execute(chip, tests[i]);
-                CU_ASSERT_TRUE(answer[i] == result);
-        }
-
-        delete_chipset(chip);
-}
-
-
-void testMANY_DIFFERENT_INSTRUCTIONS(void)
+void test_many_different_instructions(void **state)
 {
         char *tests[] = {"03 04 01 05 06 07 08 0b 09 0c 0d 09 0a 07 00",
                          "0d 08 09 08 0f 0a 01 00 0e 02 0a 06 0d 06 00"};
@@ -78,42 +50,20 @@ void testMANY_DIFFERENT_INSTRUCTIONS(void)
         for (int i = 0; i < num_tests; i++)
         {
                 int result = execute(chip, tests[i]);
-                CU_ASSERT_TRUE(answer[i] == result);
+                assert_int_equal(answer[i], result);
         }
+
+        delete_chipset(chip);
 }
 
 
 int main(void)
 {
-        // test suite
-        CU_pSuite suite = NULL;
-
-        // initialize registry
-        if (CUE_SUCCESS != CU_initialize_registry())
+        const struct CMUnitTest tests[] =
         {
-                return CU_get_error();
-        }
+            cmocka_unit_test(test_answer_three),
+            cmocka_unit_test(test_many_different_instructions),
+        };
 
-        // add suite
-        suite = CU_add_suite("Binary heap", 0, 0);
-        if (NULL == suite)
-        {
-                CU_cleanup_registry();
-                return CU_get_error();
-        }
-
-        // add tests
-        if (NULL == CU_add_test(suite, "The answer is three", testANSWER_THREE) ||
-            NULL == CU_add_test(suite, "All 4-bit instructions", testUSING_ALL_INSTRUCTIONS) ||
-            NULL == CU_add_test(suite, "Many different instructions", testMANY_DIFFERENT_INSTRUCTIONS))
-        {
-                CU_cleanup_registry();
-                return CU_get_error();
-        }
-
-        // run tests
-        CU_basic_set_mode(CU_BRM_VERBOSE);
-        CU_basic_run_tests();
-        CU_cleanup_registry();
-        return CU_get_error();
+        return cmocka_run_group_tests(tests, NULL, NULL);
 }
